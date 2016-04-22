97b997d0780852feb2631ef0db20b194d5297327
Kvinta <Kvinta30@yandex.ru>
2016-04-22 17:21:09 +0700

    Player - created

---
+++ src/io/hexlet/xo/model/Player.java
@@ -0,0 +1,24 @@
+package io.hexlet.xo.model;
+
+/**
+ * Created by Averkin on 22.04.2016.
+ */
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(final String name, final Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
