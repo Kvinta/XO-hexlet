7c07c0bcff328c702e6921b42e4c716d67f9e2ef
Kvinta <Kvinta30@yandex.ru>
2016-04-26 17:36:29 +0700

    All Edit commit (local edit)

--- src/io/hexlet/xo/model/exception/AbstractXOException.java
+++ src/io/hexlet/xo/model/exception/AbstractXOException.java
@@ -3,5 +3,5 @@ package io.hexlet.xo.model.exception;
 /**
  * Created by Averkin on 26.04.2016!
  */
-abstract class AbstractXOException extends Exception{
+public abstract class AbstractXOException extends Exception{
 }
