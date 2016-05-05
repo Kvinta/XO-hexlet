e5ecc5a835b75e6ee85b9d72991e31c7e006da00
Kvinta <Kvinta30@yandex.ru>
2016-05-05 18:09:02 +0700

    Add Winner Controller (not comlit, class Empty)

---
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -0,0 +1,14 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+
+/**
+ * Created by Averkin on 05.05.2016!
+ */
+public class WinnerController {
+
+    public Figure getWinner(final Field field) {
+
+    }
+}
